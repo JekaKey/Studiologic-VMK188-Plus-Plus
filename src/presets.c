4c5669b660c66a44594b3a05de1215930104ff8f
user811 <user811@users.noreply.github.com>
2015-12-25 00:30:43 +0300

    Bug with json calibration reading was fixed

--- src/presets.c
+++ src/presets.c
@@ -686,14 +686,16 @@ static void init_json_calibr_attr(calibrationType *cal) {
 		calibr_sliders_param_attr[i][1].addr.uint16 = &(cal->calibr[i].max_in_value);
 		strcpy(calibr_sliders_param_attr[i][2].attribute, ATTR_CAL_S_DELTA);
 		calibr_sliders_param_attr[i][2].addr.uint16 = &(cal->calibr[i].delta);
-		strcpy(calibr_sliders_param_attr[i][2].attribute, ATTR_CAL_S_DEAD);
-		calibr_sliders_param_attr[i][2].addr.uint8 = &(cal->calibr[i].dead);
-		strcpy(calibr_sliders_param_attr[i][2].attribute, ATTR_CAL_S_GAP);
-		calibr_sliders_param_attr[i][2].addr.uint8 = &(cal->calibr[i].gap);
-		for (int j = 0; j < 5; j++) {
+		strcpy(calibr_sliders_param_attr[i][3].attribute, ATTR_CAL_S_DEAD);
+		calibr_sliders_param_attr[i][3].addr.uint8 = &(cal->calibr[i].dead);
+		strcpy(calibr_sliders_param_attr[i][4].attribute, ATTR_CAL_S_GAP);
+		calibr_sliders_param_attr[i][4].addr.uint8 = &(cal->calibr[i].gap);
+		for (int j = 0; j < 3; j++) {
 			calibr_sliders_param_attr[i][j].type = t_uint16;
 		}
-		calibr_sliders_param_attr[i][3].attribute[0]=0;
+		calibr_sliders_param_attr[i][3].type = t_uint8;
+		calibr_sliders_param_attr[i][4].type = t_uint8;
+		calibr_sliders_param_attr[i][5].attribute[0]=0;
 	}
 	calibr_sliders_attr[SLIDERS_AMOUNT].attribute[0]=0;
 }

967e5150c47e8f932b5bb888002aa8f4e4cabef5
user811 <user811@users.noreply.github.com>
2016-06-11 15:37:46 +0300

    If SD error, the default preset mark is not showing now.

--- src/menu.c
+++ src/menu.c
@@ -1457,11 +1457,11 @@ static void preset_show (const presetType *pr, file_list_type *pr_list) {
 
     hd44780_goto(2, 1);
 	hd44780_write_string(line);
-
-	if (pr_list->pos == pr_list->active) {
-		hd44780_goto(1, HD44780_DISP_LENGTH);
-		hd44780_write_char(MENU_CHECK_CHAR);
-	}
+	if (!errIO)
+		if (pr_list->pos == pr_list->active) {
+			hd44780_goto(1, HD44780_DISP_LENGTH);
+			hd44780_write_char(MENU_CHECK_CHAR);
+		}
 
 	octave_shift_show();
 }
